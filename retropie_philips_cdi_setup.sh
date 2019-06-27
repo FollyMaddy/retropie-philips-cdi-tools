918c5c59afb97663f73d08e49ebc3f72d157555f
FollyMaddy <meulenf@gmx.net>
2019-06-27 19:49:03 +0200

    Add files via upload
    info on better place

--- retropie_philips_cdi_setup.sh
+++ retropie_philips_cdi_setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Author : Folkert van der Meulen
+# Date   : 01/02/2019
+#
+# Copyright 2019 Folkert van der Meulen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # ----- this script is tested only a few times, so use this at own risk ! --------
 # Preferable : do all instructions manually and read the text carefully !!!!!!!!!!!!
 
@@ -26,26 +45,6 @@ mkdir -p $HOME/RetroPie/BIOS/mame/hash
 # Make the program executable, dubbleclick and choose open in terminal.
 # Or run it from the terminal with : ./retropie_philips_cdi_setup.sh
 #
-# Author : Folkert van der Meulen
-# Date   : 01/02/2019
-#
-# Copyright 2019 Folkert van der Meulen
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#--------------------------------------
-
 
 if [ ! -f "$HOME/RetroPie/BIOS/mame/hash/cdi.xml" ]; then wget https://raw.githubusercontent.com/libretro/mame/master/hash/cdi.xml -P $HOME/RetroPie/BIOS/mame/hash; fi
 
